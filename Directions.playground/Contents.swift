d7316f8dd5fca84049dde610002b94c90e47d298
bgb0923 <bgb0923@gmail.com>
2017-02-09 00:35:54 -0500

    followed along with lab

--- Directions.playground/Contents.swift
+++ Directions.playground/Contents.swift
@@ -1,27 +1,25 @@
-func playerDidMove(_ direction: String) {
+func playerDidMove1(_ direction: String) {
     if direction == "up" ||
-       direction == "down" ||
-       direction == "left" ||
-       direction == "right" {
+        direction == "down" ||
+        direction == "left" ||
+        direction == "right" {
         print("Player moved \(direction)")
     } else {
         print("Oops! That direction doesn't make sense: \(direction)")
     }
 }
 
-playerDidMove("up")
-playerDidMove("left")
-playerDidMove("dwn")
-playerDidMove("Up")
-
-
+playerDidMove1("up")
+playerDidMove1("diagonally up")
+playerDidMove1("Up")
+playerDidMove1("down")
 
 enum Direction {
     case up
     case down
     case left
     case right
-
+    
     var excited: String {
         switch self {
         case .up:
@@ -36,18 +34,21 @@ enum Direction {
     }
 }
 
-let direction1 = Direction.up
-let direction2: Direction = .down
-var direction3: Direction = .right
-
-let direction = Direction.left
+let direction01 = Direction.up
+let direction02: Direction = .down
 
 func playerDidMove(_ direction: Direction) {
     print("Player moved \(direction.excited)")
 }
 
+playerDidMove(.left)
+
+let direction1 = Direction.up
+let direction2 : Direction = .down
+var direction3 : Direction = .right
+
 playerDidMove(direction1)
 playerDidMove(direction2)
 playerDidMove(direction3)
 
-playerDidMove(.left)
+
